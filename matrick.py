9a5dad8d9dae52ca5250133eb788fa6126a216f9
nanaz <97945936+nasrul12-sudo@users.noreply.github.com>
2024-02-23 21:00:28 +0700

    test for black vall and dormat True

--- matrick.py
+++ matrick.py
@@ -1,1 +1,13 @@
-inp = str(input())
+def matrict(n, m):
+    matc= []
+    for i in range(n):
+        inp = list(map(str, input()))
+        matc.append(inp)
+
+    print(matc)
+    
+    for i in range(len(matc)):
+        print(matc[i])
+
+inp = list(map(int, input().split()))
+matrict(inp[0], inp[1])
